549a12eddde707992408bfa4e4f76dc15f1c5f93
Trent Liu <trentliu92@gmail.com>
2018-06-13 19:12:24 -0700

    adding json as data

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/getweather', function(req, res){
 
  app.get('/waterplants', function(req, res){
     res.header("Access-Control-Allow-Origin", "*");
-    res.send("watering");
+    res.send({"data": "watering"});
 });
 
 
