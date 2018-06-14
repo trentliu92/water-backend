14a89c686073927e5feb5f24edc8fa741c1ad53c
Trent Liu <trentliu92@gmail.com>
2018-06-13 17:26:05 -0700

    init server files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const port = 3000;
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`App started on port ` + port));
+
+app.get('/', function(req, res){
+   res.send("home");
+});
+
+app.get('/getweather', function(req, res){
+    res.send("home");
+});
+
+ app.get('/waterplants', function(req, res){
+    res.send("watering");
+});
+
+
